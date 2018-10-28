94a8dae8efaab5420a62aae12109f0fa025ff33f
vikpandher <vikpandher@gmail.com>
2018-10-28 15:30:19 -0600

    Update exponents.py

--- exponents.py
+++ exponents.py
@@ -1,18 +1,13 @@
 iterations = int(input("Enter the number of iterations: "))
 x = int(input("Enter the value for x: "))
 
-if iterations <= 0:
-    print(0)
-else:
-    result = 1
-    
-    for i in range(1, iterations):
-        top = x ** i
+result = 0
+for i in range(0, iterations):
+    top = x ** i
+    bot = 1;
+    for j in range(2, i+1):
+        bot *= j
         
-        bot = 1;
-        for j in range(2, i+1):
-            bot *= j
-        
-        result += top / bot
+    result += top / bot
     
-    print(result)
+print(result)
